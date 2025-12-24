b73bc089eae8c39952ee7b09fc202349f8d4041e
Olimpio Eduardo <kamuenhoeduardo@75gmail.com>
2025-12-24 16:51:43 +0100

    refactoring sessionRoutes.js file

--- backend/src/routes/sessionRoutes.js
+++ backend/src/routes/sessionRoutes.js
@@ -11,7 +11,7 @@ router.get("/my-recent", protectRoute, getMyRecentSessions)
 
 
 router.get("/:id", protectRoute, getSessionById)
-router.get("/:id/join", protectRoute, joinSession)
+router.post("/:id/join", protectRoute, joinSession)
 router.post("/:id/end", protectRoute, endSession)
 
 export default router
